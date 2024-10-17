a2477fbcce30456fb3f14f80a20d97e18c04dbb6
Udon0729 <Satajava7782@Gmail.com>
2024-10-17 13:50:34 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -121,6 +121,25 @@ Token *tokenize(char *p)
     return head.next;
 }
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+// 受け取るポインタは入力全体を表す文字列の途中を指す
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
